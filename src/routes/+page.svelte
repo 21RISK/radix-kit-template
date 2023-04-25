b325f4e93201102b5c4efa7d2b5544b5dcf7536b
Alex Bjørlig <alexbjorlig@icloud.com>
2023-04-25 22:50:38 +0200

    added radix-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+	import { Toggle } from 'radix-svelte';
+
+	let pressed = false;
+</script>
+
+
 <h1>Welcome to this Radix-svelte Sveltekit typescript template</h1>
 <p>Visit <a href="https://github.com/TGlide/radix-svelte">radix-svelte</a> to read the documentation</p>
+
+<div class="border-gray-400 border-dotted border my-4"></div>
+
+<Toggle.Root bind:pressed class="bg-blue-400 rounded-md">
+	{pressed ? 'Pressed' : 'Not pressed'}
+</Toggle.Root>
